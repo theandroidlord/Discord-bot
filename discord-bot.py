2726ac9555056a37ad963aaa350e9c3cd5b23ece
Muhammad Ghouse <77920649+theandroidlord@users.noreply.github.com>
2024-09-06 05:25:51 +0530

    Update discord-bot.py

--- discord-bot.py
+++ discord-bot.py
@@ -4,6 +4,17 @@ from tpblite import TPB
 from dotenv import load_dotenv
 import os
 import logging
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, Render!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     
 
 # Load environment variables from .env file
